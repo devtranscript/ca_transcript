e98f20f0a0276df6c0a0ef751c66f5c1f998bb6b
u_transcript <soportetranscript@gmail.com>
2018-04-08 19:16:33 -0500

    tarea windows

---
+++ ca_transcript/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ca_transcript
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
